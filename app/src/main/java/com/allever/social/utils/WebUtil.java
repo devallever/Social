f1e496eacf39710819f0c06c1b6d7626734640dc
devallever <devallever@163.com>
2018-03-24 18:13:00 +0800

    noncode

--- app/src/main/java/com/allever/social/utils/WebUtil.java
+++ app/src/main/java/com/allever/social/utils/WebUtil.java
@@ -8,8 +8,8 @@ public class WebUtil {
     //public static String HTTP_ADDRESS = "http://39.108.9.138:8080/SocialServer";//阿里云
 
 
-    //public static String HTTP_ADDRESS = "http://192.168.43.235:8080/SocialServer";//RedMi 3
-    public static String HTTP_ADDRESS = "http://10.42.0.1:8080/SocialServer";//Linux 本机开wifi
+    public static String HTTP_ADDRESS = "http://192.168.43.235:8080/SocialServer";//RedMi 3
+    //public static String HTTP_ADDRESS = "http://10.42.0.1:8080/SocialServer";//Linux 本机开wifi
     //public static String HTTP_ADDRESS = "http://192.168.43.235:8080/SocialServer";//Linux 本机开wifi
 
    // public static String HTTP_ADDRESS = "http://27.54.249.252:8080/SocialServer";//远程服务器   27.54.249.252
